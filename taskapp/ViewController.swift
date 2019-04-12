d8d7afbc96f351fc4e62270d7478c96cb80f3a5d
USER <user@user-no-macbook-pro.local>
2019-04-12 21:04:17 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by USER on 2019/04/12.
+//  Copyright © 2019 sf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
